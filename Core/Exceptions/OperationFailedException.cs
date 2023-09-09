7fbfef81ff7a4194a4115606145dce2cc1a1a24d
Vitalik Knysh <knish.vital@gmail.com>
2023-09-09 19:33:49 +0300

    Added constructors to the custom exception according to best practices

--- Core/Exceptions/OperationFailedException.cs
+++ Core/Exceptions/OperationFailedException.cs
@@ -1,10 +1,18 @@
-﻿namespace Core.Exceptions
+﻿namespace Core.Exceptions;
+
+public class OperationFailedException : ApplicationException
 {
-    public class OperationFailedException : ApplicationException
+    public OperationFailedException()
     {
-        public OperationFailedException(string message)
-            : base(message)
-        { 
-        }
+    }
+
+    public OperationFailedException(string message)
+        : base(message)
+    { 
+    }
+
+    public OperationFailedException(string message, Exception innerException)
+        : base(message, innerException)
+    {
     }
 }
